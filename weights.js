f10dd2c36e3434f04a260ec67e199beae59529b9
truemaxdh <truemaxdh@gmail.com>
2019-11-19 13:49:27 +0900

    Update weights.js

--- weights.js
+++ weights.js
@@ -2,21 +2,18 @@ var fnWeights = {
   layers : 3,
   nodes : 16,
   wt : [],
-  init : function(base_wt) {
-    if (_wt) {
-      wt = base_wt;
-      layers = wt.length;
-      nodes = Math.sqrt(wt[0].length);
-      return;
-    }
-    
+  initWithBase : function(base_wt) {
+    wt = base_wt;
+    layers = wt.length;
+    nodes = Math.sqrt(wt[0].length);
+  },
+  initWithRndNumbers : function() {
     var n_sqr = nodes * nodes;
     for (var i = 0; i < layers; i++) {
       wt.push([]);
       for (var j = 0; j < n_sqr; j++) {
         wt[i].push(Math.rnd());
       }
-    }
-    
+    }    
   }
 }
