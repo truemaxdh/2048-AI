77d5989cbc85362429f0f23d9d470f44c1783a27
truemaxdh <truemaxdh@gmail.com>
2019-11-19 17:47:16 +0900

    Create EA.js

---
+++ EA.js
@@ -0,0 +1,10 @@
+var EA = {
+  modelNum : 100,
+  models : [],
+  createModels : function(_modelNum) {
+    modelNum = _modelNum;
+    for(var i_m = 0; i_m < modelNum; i_m++) {
+      
+    }
+  }
+}
