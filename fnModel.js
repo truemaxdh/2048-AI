07029aaefda7fe63111a3effe82f44c55fbf4992
truemaxdh <truemaxdh@gmail.com>
2019-11-19 17:50:16 +0900

    Update fnModel.js

--- fnModel.js
+++ fnModel.js
@@ -1,18 +1,18 @@
-var fnModel = {
-  wt_input : [],
-  wt_hidden : [],
-  wt_output : [],
-  initWithBaseWeights : function(wt_base_input, wt_base_hidden, wt_base_output) {
+function fnModel() {
+  this.wt_input = [];
+  this.wt_hidden = [];
+  this.wt_output = [];
+  this.initWithBaseWeights = function(wt_base_input, wt_base_hidden, wt_base_output) {
     wt_input = wt_base_input.map((x)=>x);
     wt_hidden = wt_base_hidden.map((x)=>x);
     wt_output = wt_base_output.nap((x)=>x);
-  },
-  initWithSizes : function(input_sz, hidden_sz_2d, output_sz) {
+  };
+  this.initWithSizes = function(input_sz, hidden_sz_2d, output_sz) {
     _fill_wt_rndNums(wt_input, 1, input_sz, hidden_sz_2d[1]);
     _fill_wt_rndNums(wt_hidden, hidden_sz_2d[0], hidden_sz_2d[1], hidden_sz_2d[1]);
     _fill_wt_rndNums(wt_output, 1, hidden_sz_2d[1], output_sz);
-  },
-  _fill_wt_rndNums : function(wt, layer, row, col) {
+  };
+  this._fill_wt_rndNums = function(wt, layer, row, col) {
     for (var l = 0; l < layer; l++) {
       wt.push([]);
       for (var r = 0; r < row; r++) {
@@ -22,8 +22,8 @@ var fnModel = {
         }
       }
     }
-  },
-  forward : function(inputs) {
+  };
+  this.forward = function(inputs) {
     if (inputs.length != wt_input[0].length) {
       console.log("input size is incorrect!. The size should be " + 
                   wt_input[0].length + ", but it is " + inputs.length);
@@ -36,8 +36,8 @@ var fnModel = {
     }
     var outputs = _mat_mul(arr_calc, wt_output[0]);
     return outputs;
-  },
-  _mat_mul : function(arr_in, arr_wt) {
+  };
+  this._mat_mul = function(arr_in, arr_wt) {
     var arr_out = new Array(arr_wt.length).fill(0);
     for (var i_in = 0; i_in < arr_in.length; i_in++) {
       for (var i_wt = 0; i_wt < arr_wt.length; i_wt++) {
@@ -45,5 +45,5 @@ var fnModel = {
       }
     }
     return arr_out;
-  }
+  };
 }
