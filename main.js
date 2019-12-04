c8d97b44d94cd3c5693a1913cce9f853952d77cf
unknown <truemaxdh@gmail.com>
2019-12-04 13:19:36 +0900

    bug fixed

--- main.js
+++ main.js
@@ -81,9 +81,9 @@ function moveOnce() {
   }
   
   if (compArrVals(last_inputs, inputs)) {
-	  console.log(last_inputs);
-	  console.log(inputs);
-	  alert("go to next model");
+	  //console.log(last_inputs);
+	  //console.log(inputs);
+	  //alert("go to next model");
 	  last_inputs = null;
 	  return false;
   }
@@ -127,14 +127,14 @@ function isMovable(_in_arr16, _dir) {
 	var dx = (_dir - 1) % 2;
 	var dy = (_dir - 2) % 2;
 	var b_movable = false;
-	console.log(dx + "," + dy);
+	//console.log(dx + "," + dy);
 	for (var i = 0; i < _in_arr16.length; i++) {
 		if (!_in_arr16[i]) continue;
 		var nx = (i / 4 | 0) + dx;
 		var ny = (i % 4) + dy;
 		if (nx < 0 || nx >= 4 || ny < 0 || ny >= 4) continue;
 		var i_new = 4 * nx + ny;
-		console.log(i + ":" + _in_arr16[i] + "->" + i_new + ":" + _in_arr16[i_new]);
+		//console.log(i + ":" + _in_arr16[i] + "->" + i_new + ":" + _in_arr16[i_new]);
 		if (!_in_arr16[i_new] || _in_arr16[i] == _in_arr16[i_new]) {
 			b_movable = true;
 			break;
