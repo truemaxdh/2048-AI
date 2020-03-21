505083ba1531da33fe3f37d7e4d51a706d63ac34
truemaxdh <truemaxdh@gmail.com>
2020-03-21 19:04:13 +0900

    Update common.js

--- common/common.js
+++ common/common.js
@@ -63,12 +63,33 @@ function connectToGame() {
       document.gameFrm.LocalStorageManager);
   }
   console.log(gameMgr);*/
-  try {
-  console.log(getEventListeners(document.gameFrm.document, 'keydown'));
-  } catch(err){}
-  try {
-  console.log(getEventListeners(document.gameFrm, 'keydown'));              
-  } catch(err){}
+  const listeners = (function listAllEventListeners() {
+  let elements = [];
+  const allElements = document.querySelectorAll('*');
+  const types = [];
+  for (let ev in window) {
+    if (/^on/.test(ev)) types[types.length] = ev;
+  }
+
+  for (let i = 0; i < allElements.length; i++) {
+    const currentElement = allElements[i];
+    for (let j = 0; j < types.length; j++) {
+      if (typeof currentElement[types[j]] === 'function') {
+        elements.push({
+          "node": currentElement,
+          "listeners": [ {
+            "type": types[j],
+            "func": currentElement[types[j]].toString(),
+          }]
+        });
+      }
+    }
+  }
+
+  return elements.filter(element => element.listeners.length)
+})();
+
+console.table(listeners);
 }
 
 
